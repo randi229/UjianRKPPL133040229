87139c82c85c0b551825991a1e4a7ee8ba250de2
randi229 <randi.133040229@mail.unpas.ac.id>
2016-05-13 14:18:12 +0700

    update baru

---
+++ src/main/java/ujian/Main.java
@@ -0,0 +1,9 @@
+package ujian;
+
+public class Main {
+
+	public static void main(String[] args) {
+		UjianLog log = new UjianLog();
+		log.ShowLog();
+	}
+}
---
+++ src/main/java/ujian/UjianLog.java
@@ -0,0 +1,13 @@
+package ujian;
+
+
+public class UjianLog {
+	
+	public void ShowLog(){
+		System.out.println("2016-05-13 14:12:30 DEBUG Log4JDemo : 19 - Ini adalah debug message : Contoh Log4J");
+		System.out.println("2016-05-13 14:12:30 INFO  Log4JDemo : 23 - Ini adalah info message  : Contoh Log4J");
+		System.out.println("2016-05-13 14:12:30 WARN  Log4JDemo : 26 - Ini adalah warn  message : Contoh Log4J");
+		System.out.println("2016-05-13 14:12:30 ERROR Log4JDemo : 27 - Ini adalah error message : Contoh Log4J");
+		System.out.println("2016-05-13 14:12:30 FATAL Log4JDemo : 28 - Ini adalah fatal message : Contoh Log4J");
+	}
+}
